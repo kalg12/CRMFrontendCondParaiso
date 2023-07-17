4b4f85e8e522b7f79d36b1d5ce78ce4c6f800c40
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-07-17 10:22:19 -0600

    ThemeProvider addded

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Admin from "./dashboard/Admin";
+import ThemeProvider from "@/app/utils/ThemeContext";
 
 const admin = () => {
-  return <Admin />;
+  return (
+    <ThemeProvider>
+      <Admin />
+    </ThemeProvider>
+  );
 };
 
 export default admin;

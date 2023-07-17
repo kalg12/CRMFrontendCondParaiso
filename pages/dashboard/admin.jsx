29f04a2605e71120773e60605e599933b4de1fb3
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-07-17 11:03:43 -0600

    Chards in cards added by components

--- pages/dashboard/admin.jsx
+++ pages/dashboard/admin.jsx
@@ -3,6 +3,21 @@ import Header from "@/components/dashboard/Header";
 import Sidebar from "@/components/dashboard/Sidebar";
 import WelcomeBanner from "@/components/dashboard/WelcomeBanner";
 import DashboardAvatars from "@/components/dashboard/DashboardAvatars";
+import FilterButton from "@/components/dashboard/DropdownFilter";
+import Datepicker from "@/components/dashboard/Datepicker";
+import DashboardCard01 from "@/components/dashboard/DashboardCard01";
+import DashboardCard02 from "@/components/dashboard/DashboardCard02";
+import DashboardCard03 from "@/components/dashboard/DashboardCard03";
+import DashboardCard04 from "@/components/dashboard/DashboardCard04";
+import DashboardCard05 from "@/components/dashboard/DashboardCard05";
+import DashboardCard06 from "@/components/dashboard/DashboardCard06";
+import DashboardCard07 from "@/components/dashboard/DashboardCard07";
+import DashboardCard08 from "@/components/dashboard/DashboardCard08";
+import DashboardCard09 from "@/components/dashboard/DashboardCard09";
+import DashboardCard10 from "@/components/dashboard/DashboardCard10";
+import DashboardCard11 from "@/components/dashboard/DashboardCard11";
+import DashboardCard12 from "@/components/dashboard/DashboardCard12";
+import DashboardCard13 from "@/components/dashboard/DashboardCard13";
 import "tailwindcss/tailwind.css";
 
 const Admin = () => {
@@ -12,30 +27,69 @@ const Admin = () => {
     <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-      {/*  Content area */}
+
+      {/* Content area */}
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-        {/* Site header */}
+        {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-            {/* Welcome Banner */}
+            {/* Welcome banner */}
             <WelcomeBanner />
+
             {/* Dashboard actions */}
             <div className="sm:flex sm:justify-between sm:items-center mb-8">
-              {/* Left Avatars */}
+              {/* Left: Avatars */}
               <DashboardAvatars />
-              {/* Right Actions */}
+
+              {/* Right: Actions */}
               <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-                {/* Date */}
-                <div className="text-sm text-gray-500 dark:text-gray-400">
-                  {new Date().toLocaleDateString("es-ES", {
-                    weekday: "long",
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  })}
-                </div>
+                {/* Filter button */}
+                <FilterButton />
+                {/* Datepicker built with flatpickr */}
+                <Datepicker />
+                {/* Add view button */}
+                <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
+                  <svg
+                    className="w-4 h-4 fill-current opacity-50 shrink-0"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                  </svg>
+                  <span className="hidden xs:block ml-2">Add view</span>
+                </button>
               </div>
+            </div>
+
+            {/* Cards */}
+            <div className="grid grid-cols-12 gap-6">
+              {/* Line chart (Acme Plus) */}
+              <DashboardCard01 />
+              {/* Line chart (Acme Advanced) */}
+              <DashboardCard02 />
+              {/* Line chart (Acme Professional) */}
+              <DashboardCard03 />
+              {/* Bar chart (Direct vs Indirect) */}
+              <DashboardCard04 />
+              {/* Line chart (Real Time Value) */}
+              <DashboardCard05 />
+              {/* Doughnut chart (Top Countries) */}
+              <DashboardCard06 />
+              {/* Table (Top Channels) */}
+              <DashboardCard07 />
+              {/* Line chart (Sales Over Time) */}
+              <DashboardCard08 />
+              {/* Stacked bar chart (Sales VS Refunds) */}
+              <DashboardCard09 />
+              {/* Card (Customers) */}
+              <DashboardCard10 />
+              {/* Card (Reasons for Refunds) */}
+              <DashboardCard11 />
+              {/* Card (Recent Activity) */}
+              <DashboardCard12 />
+              {/* Card (Income/Expenses) */}
+              <DashboardCard13 />
             </div>
           </div>
         </main>

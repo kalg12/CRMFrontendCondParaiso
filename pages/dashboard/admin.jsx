ddd01320102b0065bf50d9372d7aa92f6a0c0f4c
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-07-29 01:19:36 -0600

    Children layout

--- pages/dashboard/admin.jsx
+++ pages/dashboard/admin.jsx
@@ -3,24 +3,11 @@ import Header from "@/components/dashboard/Header";
 import Sidebar from "@/components/dashboard/Sidebar";
 import WelcomeBanner from "@/components/dashboard/WelcomeBanner";
 import DashboardAvatars from "@/components/dashboard/DashboardAvatars";
-import DashboardCard01 from "@/components/dashboard/DashboardCard01";
-import DashboardCard02 from "@/components/dashboard/DashboardCard02";
-import DashboardCard03 from "@/components/dashboard/DashboardCard03";
-import DashboardCard04 from "@/components/dashboard/DashboardCard04";
-import DashboardCard05 from "@/components/dashboard/DashboardCard05";
-import DashboardCard06 from "@/components/dashboard/DashboardCard06";
-import DashboardCard07 from "@/components/dashboard/DashboardCard07";
-import DashboardCard08 from "@/components/dashboard/DashboardCard08";
-import DashboardCard09 from "@/components/dashboard/DashboardCard09";
-import DashboardCard10 from "@/components/dashboard/DashboardCard10";
-import DashboardCard11 from "@/components/dashboard/DashboardCard11";
-import DashboardCard12 from "@/components/dashboard/DashboardCard12";
-import DashboardCard13 from "@/components/dashboard/DashboardCard13";
 import { useRouter } from "next/router";
 import { Helmet } from "react-helmet";
 import "tailwindcss/tailwind.css";
 
-const Admin = () => {
+const Admin = ({children}) => {
   const router = useRouter();
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
@@ -98,36 +85,8 @@ const Admin = () => {
                     </span>
                   </div>
                 </div>
-
-                {/* Cards */}
-                <div className="grid grid-cols-12 gap-6">
-                  {/* Line chart (Acme Plus) */}
-                  <DashboardCard01 />
-                  {/* Line chart (Acme Advanced) */}
-                  <DashboardCard02 />
-                  {/* Line chart (Acme Professional) */}
-                  <DashboardCard03 />
-                  {/* Bar chart (Direct vs Indirect) */}
-                  <DashboardCard04 />
-                  {/* Line chart (Real Time Value) */}
-                  <DashboardCard05 />
-                  {/* Doughnut chart (Top Countries) */}
-                  <DashboardCard06 />
-                  {/* Table (Top Channels) */}
-                  <DashboardCard07 />
-                  {/* Line chart (Sales Over Time) */}
-                  <DashboardCard08 />
-                  {/* Stacked bar chart (Sales VS Refunds) */}
-                  <DashboardCard09 />
-                  {/* Card (Customers) */}
-                  <DashboardCard10 />
-                  {/* Card (Reasons for Refunds) */}
-                  <DashboardCard11 />
-                  {/* Card (Recent Activity) */}
-                  <DashboardCard12 />
-                  {/* Card (Income/Expenses) */}
-                  <DashboardCard13 />
-                </div>
+                {/* Children */}
+                { children }
               </div>
             </main>
           </div>
